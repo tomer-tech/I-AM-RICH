c3e9b28ee2b9becf733d952677012a508c7bcf8c
Ankhe Tomer <ankhe.tomer@gmail.com>
2023-01-26 13:31:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# I-AM-RICH
+ Secon App
---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by Ankhe Tomer on 12/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
